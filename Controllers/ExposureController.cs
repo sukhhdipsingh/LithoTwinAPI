b178a02de7e6238341680e1633f3bdcd9a66b859
Sukhdip Singh <sukhdip944@gmail.com>
2026-03-20 15:10:22 +0100

    feat: update ExposureController API endpoints

--- Controllers/ExposureController.cs
+++ Controllers/ExposureController.cs
@@ -13,9 +13,6 @@ public class ExposureController : ControllerBase
     public ExposureController(ExposureService exposure) => _exposure = exposure;
 
     [HttpPost("run")]
-    [ProducesResponseType(typeof(ExposureResult), StatusCodes.Status200OK)]
-    [ProducesResponseType(StatusCodes.Status404NotFound)]
-    [ProducesResponseType(StatusCodes.Status409Conflict)]
     public async Task<IActionResult> RunExposure([FromBody] ExposureRequest req)
     {
         try
